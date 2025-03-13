aac05b53de13b713e17e2fc4548b9f9f188f87ad
Julian Ranieri <jumpingafterrain@gmail.com>
2025-03-13 10:32:42 -0700

    venv, dotenv, requirements, openai, print haiku

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+# Setup dotenv 
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+client = OpenAI(
+  api_key=OPENAI_API_KEY
+)
+
+completion = client.chat.completions.create(
+  model="gpt-4o-mini",
+  store=True,
+  messages=[
+    {"role": "user", "content": "write a haiku"}
+  ]
+)
+
+print(completion.choices[0].message);
