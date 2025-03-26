32e7b075e7dc2caf0f52144d2b4795ee691b22d2
nightangelblade <sean.syc.wright@gmail.com>
2025-03-26 12:23:55 -0700

    Turned GPT haiku prompt call into function get_gpt_haiku.

--- main.py
+++ main.py
@@ -8,14 +8,20 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 REPO_PATH = os.getenv("REPO_PATH")
 
-client = OpenAI(api_key=OPENAI_API_KEY)
 
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    store=True,
-    messages=[{"role": "user", "content": "write a haiku"}],
-)
+def get_gpt_haiku() -> list:
+    client = OpenAI(api_key=OPENAI_API_KEY)
 
-print(completion.choices[0].message)
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        store=True,
+        messages=[{"role": "user", "content": "write a haiku"}],
+    )
+
+    completion_list = completion.choices[0].message.content.split("\n")
+    completion_list = [line.rstrip() for line in completion_list]
+    return completion_list
+
+print(get_gpthaiku())
 
 git_controller = GitController(REPO_PATH)
