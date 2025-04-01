d1ff2f7279cd3cc014241135aa24ca4446d163c8
Julian Ranieri <jumpingafterrain@gmail.com>
2025-04-01 12:02:19 -0700

    site generator and templates

---
+++ site_generator.py
@@ -0,0 +1,15 @@
+
+from jinja2 import Environment, FileSystemLoader
+
+
+class SiteGenerator:
+    def __init__(self, haikus: list):
+        env = Environment(loader=FileSystemLoader('.'))
+        self.haikus = haikus
+
+    def generate(self):
+        template = self.env.get_template('template.html')
+        output = template.render(haikus=self.haikus)
+
+        with open('index.html', 'w', encoding='utf-8') as f:
+            f.write(output)
